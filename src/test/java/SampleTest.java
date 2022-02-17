fa218e9be794aac18db09b3145100a460c876e5c
JinHo Kim <49276666+wlsh44@users.noreply.github.com>
2022-02-18 01:32:54 +0900

    test: 테스트 코드 추가 #1

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,57 @@
+import Calculator.CalculateHandler;
+import Calculator.CalculateHandlerImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SampleTest {
+
+    private final CalculateHandler calculateHandler = new CalculateHandlerImpl();
+
+    @Test
+    void sample_test() {
+        int target = 1 + 2;
+        assertThat(target).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("1 + 2 * 3 - 4 / 5 = 1")
+    void calculate_test1() {
+        String expression = "1 + 2 * 3 - 4 / 5";
+
+        int res = calculateHandler.calculate(expression);
+
+        assertThat(res).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("2 + 3 * 4 / 2 = 10")
+    void calculate_test2() {
+        String expression = "2 + 3 * 4 / 2";
+
+        int res = calculateHandler.calculate(expression);
+
+        assertThat(res).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("1 + 0 - 10 * 5 / 9 = -5")
+    void calculate_test3() {
+        String expression = "1 + 0 - 10 * 5 / 9";
+
+        int res = calculateHandler.calculate(expression);
+
+        assertThat(res).isEqualTo(-5);
+    }
+
+    @Test
+    @DisplayName("10 + 20 * 30 - 40 / 10 = 86")
+    void calculate_test4() {
+        String expression = "10 + 20 * 30 - 40 / 10";
+
+        int res = calculateHandler.calculate(expression);
+
+        assertThat(res).isEqualTo(86);
+    }
+}
