392d814c1a6769d47d0e910c80bfb8b563dacc14
JinHo Kim <49276666+wlsh44@users.noreply.github.com>
2022-02-18 01:14:12 +0900

    feat: 연산자 상수 추가 #1

---
+++ src/main/java/Calculator/Operator.java
@@ -0,0 +1,8 @@
+package Calculator;
+
+public class Operator {
+    public static final String PLUS = "+";
+    public static final String MINUS = "-";
+    public static final String MULTIPLY = "*";
+    public static final String DIVIDE = "/";
+}
