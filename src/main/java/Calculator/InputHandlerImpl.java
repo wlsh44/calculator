6fbbe8ad4e700c456f2c8c12e5fd9337f8abc721
wlsh44 <k87913j@gmail.com>
2022-02-21 22:19:17 +0900

    chore:  Scanner 객체 맴버 변수로 변경 #1

--- src/main/java/Calculator/InputHandlerImpl.java
+++ src/main/java/Calculator/InputHandlerImpl.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class InputHandlerImpl implements InputHandler {
 
+    private final Scanner scanner = new Scanner(System.in);
+
     @Override
     public String inputData() {
-        Scanner scanner = new Scanner(System.in);
-
         return scanner.nextLine();
     }
 }
