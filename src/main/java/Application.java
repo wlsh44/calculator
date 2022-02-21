fd7dcbdabf9185cfa4e0e89f75817aab98d1e7a5
wlsh44 <k87913j@gmail.com>
2022-02-21 22:24:53 +0900

    feat: 실행 부분 구현 #1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,11 @@
+import Calculator.*;
+
 public class Application {
     public static void main(String[] args) {
+        CalculateHandler calculateHandler = new ArithmeticCalculateHandler();
+        InputHandler inputHandler = new SystemInputHandler();
+        Calculator calculator = new Calculator(inputHandler, calculateHandler);
 
+        System.out.println(calculator.calculate());
     }
 }
