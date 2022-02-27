9aff9c028d0c5ba78eea683b2928804f8cf6f689
wlsh44 <k87913j@gmail.com>
2022-02-27 15:56:40 +0900

    test: 테스트 안티 패턴 삭제 #1

--- src/test/java/Calculator/CalculatorTest.java
+++ src/test/java/Calculator/CalculatorTest.java
@@ -1,9 +1,12 @@
 package Calculator;
 
 import Calculator.dto.CalculatorDto;
+import Calculator.handler.IO.InputHandler;
+import Calculator.handler.IO.OutputHandler;
 import Calculator.handler.IO.SystemInputHandler;
 import Calculator.handler.IO.SystemOutputHandler;
 import Calculator.handler.calculate.ArithmeticCalculateHandler;
+import Calculator.handler.calculate.CalculateHandler;
 import Calculator.handler.calculate.DequeExpressionIterator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -20,7 +23,10 @@ class CalculatorTest {
     public void initCalculator(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-        calculator = new Calculator(new SystemInputHandler(), new SystemOutputHandler(), new ArithmeticCalculateHandler(new DequeExpressionIterator()));
+        CalculateHandler calculateHandler = new ArithmeticCalculateHandler(new DequeExpressionIterator());
+        InputHandler inputHandler = new SystemInputHandler();
+        OutputHandler outputHandler = new SystemOutputHandler();
+        calculator = new Calculator(inputHandler, outputHandler, calculateHandler);
     }
 
     @Nested
