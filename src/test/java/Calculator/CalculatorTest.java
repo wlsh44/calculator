ba8fe20a781b0e7edda842c76544d4eed26a4073
wlsh44 <k87913j@gmail.com>
2022-03-01 01:04:59 +0900

    test: 테스트용 input handler 추가 #1

--- src/test/java/Calculator/CalculatorTest.java
+++ src/test/java/Calculator/CalculatorTest.java
@@ -2,9 +2,7 @@ package Calculator;
 
 import Calculator.dto.ExpressionDto;
 import Calculator.dto.ResultDto;
-import Calculator.handler.IO.InputHandler;
 import Calculator.handler.IO.OutputHandler;
-import Calculator.handler.IO.SystemInputHandler;
 import Calculator.handler.IO.SystemOutputHandler;
 import Calculator.handler.calculate.ArithmeticCalculateHandler;
 import Calculator.handler.calculate.CalculateHandler;
@@ -21,13 +19,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CalculatorTest {
 
     Calculator calculator;
+    CalculateHandler calculateHandler = new ArithmeticCalculateHandler(new DequeExpressionIterator());
+    TestSystemInputHandler inputHandler = new TestSystemInputHandler();
+    OutputHandler outputHandler = new SystemOutputHandler();
 
-    public void initCalculator(String input) {
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        CalculateHandler calculateHandler = new ArithmeticCalculateHandler(new DequeExpressionIterator());
-        InputHandler inputHandler = new SystemInputHandler();
-        OutputHandler outputHandler = new SystemOutputHandler();
+    @BeforeEach
+    public void initCalculator() {
         calculator = new Calculator(inputHandler, outputHandler, calculateHandler);
     }
 
@@ -39,7 +36,7 @@ class CalculatorTest {
         @DisplayName("1 + 2 - 3 / 4 * 5 가 입력된 경우")
         void inputTest() {
             String input = "1 + 2 - 3 / 4 * 5";
-            initCalculator(input);
+            inputHandler.setScanner(input);
 
             ExpressionDto dto = calculator.inputExpression();
 
@@ -55,7 +52,7 @@ class CalculatorTest {
         @DisplayName("1 + 2 * 3 - 4 / 5 = 1")
         void calculateTest1() {
             String input = "1 + 2 * 3 - 4 / 5";
-            initCalculator(input);
+            inputHandler.setScanner(input);
 
             ExpressionDto dto = calculator.inputExpression();
             ResultDto calculate = calculator.calculate(dto);
@@ -67,7 +64,7 @@ class CalculatorTest {
         @DisplayName("2 + 3 * 4 / 2 = 10")
         void calculateTest2() {
             String input = "2 + 3 * 4 / 2";
-            initCalculator(input);
+            inputHandler.setScanner(input);
 
             ExpressionDto dto = calculator.inputExpression();
             ResultDto calculate = calculator.calculate(dto);
@@ -91,7 +88,7 @@ class CalculatorTest {
         @DisplayName("1 + 2 * 3 - 4 / 5 = 1 결과 출력")
         void outputTest1() {
             String input = "1 + 2 * 3 - 4 / 5";
-            initCalculator(input);
+            inputHandler.setScanner(input);
 
             ExpressionDto dto = calculator.inputExpression();
             ResultDto resultDto = calculator.calculate(dto);
@@ -104,7 +101,7 @@ class CalculatorTest {
         @DisplayName("2 + 3 * 4 / 2 = 10 결과 출력")
         void outputTest2() {
             String input = "2 + 3 * 4 / 2";
-            initCalculator(input);
+            inputHandler.setScanner(input);
 
             ExpressionDto dto = calculator.inputExpression();
             ResultDto resultDto = calculator.calculate(dto);
@@ -113,5 +110,4 @@ class CalculatorTest {
             assertThat(outputStream.toString().trim()).isEqualTo("2 + 3 * 4 / 2 = 10");
         }
     }
-
 }
