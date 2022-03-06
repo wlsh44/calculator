c33d291ed11c901c3efc233b13ca620fc39767f6
wlsh44 <k87913j@gmail.com>
2022-03-06 13:25:55 +0900

    test: 테스트 수정 #1

--- src/test/java/Calculator/handler/calculate/DequeExpressionIteratorTest.java
+++ src/test/java/Calculator/handler/calculate/DequeExpressionIteratorTest.java
@@ -84,20 +84,9 @@ class DequeExpressionIteratorTest {
     @DisplayName("예외 테스트")
     class ExceptionTest {
 
-        @Test
-        @DisplayName("null 값 입력")
-        void invalidExpressionTest() {
-            String expression = null;
-            Exception exception = assertThrows(NullPointerException.class, () -> iterator.splitExpression(expression));
-
-            String expected = "null 값 입력";
-
-            assertThat(exception.getMessage()).isEqualTo(expected);
-        }
-
         private Stream<Arguments> provideInvalidSplitExpression() {
             return Stream.of(
-                    Arguments.of(null, new NullPointerException("null 값 입력")),
+                    Arguments.of(null, new InvalidExpressionException("null 값 입력")),
                     Arguments.of(" ", new InvalidExpressionException("올바르지 않은 수식")),
                     Arguments.of("1 + 1 1 - 2 / 4", new InvalidExpressionException("올바르지 않은 수식")),
                     Arguments.of("1+ 2 - 3", new InvalidExpressionException("올바르지 않은 수식")),
@@ -113,7 +102,7 @@ class DequeExpressionIteratorTest {
         @ParameterizedTest(name = "{index} => {0} = {1}")
         @MethodSource("provideInvalidSplitExpression")
         @DisplayName("올바르지 않은 수식")
-        void invalidExpressionTest2(String expression, Exception expected) {
+        void invalidExpressionTest(String expression, Exception expected) {
             Exception exception = assertThrows(InvalidExpressionException.class, () -> iterator.splitExpression(expression));
 
             assertThat(exception.getMessage()).isEqualTo(expected.getMessage());
